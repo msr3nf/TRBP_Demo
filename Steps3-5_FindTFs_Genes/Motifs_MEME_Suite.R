477d22acda01fe6a3c4cb075cae10417fd797cb1
Mahima Reddy <62672439+msr3nf@users.noreply.github.com>
2025-03-22 19:15:03 -0500

    Update Motifs_MEME_Suite.R

--- Steps3-5_FindTFs_Genes/Motifs_MEME_Suite.R
+++ Steps3-5_FindTFs_Genes/Motifs_MEME_Suite.R
@@ -1,18 +1,18 @@
 #####meme suite in R#####
 BiocManager::install("memes")
-library(memes)
+BiocManager::install("JASPAR2022")
+{
+ library(memes)
 library(magrittr)
 library(Biostrings)
 library(universalmotif)
-BiocManager::install("JASPAR2022")
 library(JASPAR2022)
 library(TFBSTools)
-packageVersion("TFBSTools")
 
-curated.list <- read.csv("/Users/mahimareddy/Downloads/scRNA_up_groups.2.csv")
+curated.list <- read.csv("/Users/Downloads/scRNA_up_groups.2.csv")
 curated.list <- subset(curated.list, subset=avg_log2FC >= log2(1.5)) #yields 30 genes 
  #identify PPR regions based on output of Step 1
-WholeGenome_List <- read.csv("/Users/mahimareddy/Desktop/WholeGenome_List_mm10_20kPPRseq_03222025.trimmed.csv")
+WholeGenome_List <- read.csv("/Users/Desktop/WholeGenome_List_mm10_20kPPRseq_03222025.trimmed.csv")
 curated.genomes <- subset(WholeGenome_List, external_gene_name %in% curated.list$gene)
 curated.genomes$PPR_sequence <- as.character(curated.genomes$PPR_sequence)
 curated.genomes <- curated.genomes[!duplicated(curated.genomes$PPR_sequence),] #remove any duplicate sequences
@@ -28,13 +28,20 @@ meme_results <- runMeme(fasta_sequences, alph = "dna", parse_genomic_coord = FAL
 #now use tomtom to get predicted TFs (vignette: "Motif Comparison using TomTom")
 motifs <- lapply(meme_results$name, function(x) create_motif(x))
 motifs
+sapply(motifs, class)
 #specify the database to use for tomtom
 opts <- list(species = 10090) #this is for mouse; you can change this for species of interest
 jaspar_motifs <- getMatrixSet(JASPAR2022, opts)
-options(meme_db = jaspar_motifs)
-jaspar_file <- tempfile(fileext = ".meme")
-export_meme(jaspar_motifs, jaspar_file)
-class(motifs)
-sapply(motifs, class)
 
-tomtom_results <- runTomTom(motifs, database = jaspar_motifs)
+jaspar_motifs_file <- tempfile(fileext = ".meme")
+write_meme(jaspar_motifs, jaspar_motifs_file)
+options(meme_db = jaspar_motifs_file)
+
+
+tomtom_results <- runTomTom(motifs)
+
+#Extract "best match name" (the TF) for all motifs in tomtom_results --> this is the final TF list
+best_match_TF_names <- sapply(tomtom_results, function(x) x$best_match_name)
+best_match_TF_df <- data.frame(best_match_name = unique(unlist(best_match_names)))
+                              
+}
